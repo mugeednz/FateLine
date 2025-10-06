5a8af7cb0230f1148ceeb404c544913d0c77c5fb
Müge Deniz <>
2025-10-06 17:32:09 +0300

    Initial commit

---
+++ Desktop/APP HELPER KLASORLERI/muge-apps/fateline/fateline/fateline/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fateline
+//
+//  Created by Müge Deniz on 6.10.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
