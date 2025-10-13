ae463444c00ab5e97c97f4789d576cba615e3cba
Müge Deniz <>
2025-10-13 20:52:49 +0300

    Onboarding and splash pages opened

---
+++ Desktop/APP HELPER KLASORLERI/muge-apps/fateline/fateline/fateline/View Controllers/Onboarding/OnboardingOneViewController.swift
@@ -0,0 +1,29 @@
+//
+//  OnboardingOneViewController.swift
+//  fateline
+//
+//  Created by Müge Deniz on 12.10.2025.
+//
+
+import UIKit
+
+class OnboardingOneViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
